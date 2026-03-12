9369bb91e5a72fc064012e9a87cdf67a2a71a08b
Emry Daniel <emrydaniel1@gmail.com>
2026-03-12 12:35:09 +0800

    Add basic getters and simple modifying and filtering of statuses

--- src/main/java/seedu/interntrackr/command/FilterCommand.java
+++ src/main/java/seedu/interntrackr/command/FilterCommand.java
@@ -1,5 +1,6 @@
 package seedu.interntrackr.command;
 
+import seedu.interntrackr.model.Application;
 import seedu.interntrackr.model.ApplicationList;
 import seedu.interntrackr.storage.Storage;
 import seedu.interntrackr.ui.Ui;
@@ -18,5 +19,19 @@ public class FilterCommand extends Command {
     @Override
     public void execute(ApplicationList applications, Ui ui, Storage storage) throws InternTrackrException {
         // TODO: Filter applications by status and display results
+        System.out.println("Here are the applications with status: " + this.status);
+        int matchCount = 0;
+
+        for (int i = 0; i < applications.getSize(); i++) {
+            Application app = applications.getApplication(i);
+            if (app.getStatus().equalsIgnoreCase(this.status)) {
+                System.out.println((i + 1) + ". " + app.toString());
+                matchCount++;
+            }
+        }
+
+        if (matchCount == 0) {
+            System.out.println("No applications found with status: " + this.status);
+        }
     }
 }
